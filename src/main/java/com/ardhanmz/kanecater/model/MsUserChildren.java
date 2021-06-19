f6bfd5728041f33085f301736c5b7ec265cc7569
ardhanmz <ardhan.maulana@gmail.com>
2021-06-19 19:42:33 +0700

    Add Children Model

--- src/main/java/com/ardhanmz/kanecater/model/MsUserChildren.java
+++ src/main/java/com/ardhanmz/kanecater/model/MsUserChildren.java
@@ -1,5 +1,46 @@
 package com.ardhanmz.kanecater.model;
 
-public class MsUserChildren {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "ms_user_children")
+public class MsUserChildren {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column
+	private int UuidMsUserChildren;
+	
+	
+	@Column
+	private int UuidMsUser;
+	
+	@Column
+	private String childrenName;
+	
+	@Column
+	private String birthDate;
+	
+	@Column
+	private String sex;
+	
+	@Column
+	private String weight;
+	
+	@Column
+	private String height;
+	
+	@Column
+	private String school;
+	
+	@Column
+	private String classSchool;
+	
+	@Column
+	private Byte[] image;
 }
